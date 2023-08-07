2295f6fd67c5e055967e2ba9ec1c7bc9eef08da3
Emanuela Trzop <emanuela.trzop@gmail.com>
2023-08-07 16:06:11 +0200

    carousel and footer

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,7 +1,9 @@
 const navEl = document.querySelector('.navbar');
 
 window.addEventListener('scroll', () => {
-    if (window.scrollY >= 56){
+    if (window.scrollY >= 56) {
         navEl.classList.add('navbar-scolled');
+    } else if (window.scrollY < 56) {
+        navEl.classList.remove('navbar-scolled')
     }
 })
